03783bda802495f9a8ed06ceafb6fdc76b63173b
simon <minimalistadress@gmail.com>
2026-07-08 18:52:26 +0200

    chore: Remove unnecessary default comments

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"embed"
 	_ "embed"
-	//"flag"
 	"fmt"
 	"log"
 	"os"
@@ -16,24 +15,12 @@ import (
 	"tavlio/video"
 )
 
-// Wails uses Go's `embed` package to embed the frontend files into the binary.
-// Any files in the frontend/dist folder will be embedded into the binary and
-// made available to the frontend.
-// See https://pkg.go.dev/embed for more information.
-
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func init() {
-	// Register a custom event whose associated data type is string.
-	// This is not required, but the binding generator will pick up registered events
-	// and provide a strongly typed JS/TS API for them.
-	application.RegisterEvent[string]("time")
 }
 
-// main function serves as the application's entry point. It initializes the application, creates a window,
-// and starts a goroutine that emits a time-based event every second. It subsequently runs the application and
-// logs any error that might occur.
 func main() {
     os.MkdirAll("data/screenshots", 0755)
 	os.MkdirAll("data/videos", 0755)
